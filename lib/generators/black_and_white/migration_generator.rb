a5599cdbeabd41aed1c63c7665b745f394d173e6
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-06-07 15:57:46 +0300

    add cleaner version of migration generator

--- lib/generators/black_and_white/migration_generator.rb
+++ lib/generators/black_and_white/migration_generator.rb
@@ -1,9 +1,23 @@
+#require 'active_record/generators/migration'
 require 'rails/generators/active_record'
 
 module BlackAndWhite
   module Generators
-    class MigrationGenerator < ActiveRecord::Generators::Base
+    class MigrationGenerator < Rails::Generators::Base # ActiveRecord::Generators::Base
       include Rails::Generators::Migration
+      # include ActiveRecord::Generators::Migration
+      # For some reason including ActiveRecord::Generators::Migration causes
+      # the generator to not appear when calling `rails g`.
+      # ActiveRecord::Generators::Migration already implements the `next_migration_number` and
+      # includes Rails::Generators::Migration in itself, so it woulda been a lot easier to just include
+      # it here. However this does not seem possible so we just evaluate the same method directly in
+      # Rails::Generators::Migration
+      Rails::Generators::Migration::ClassMethods.class_eval do
+        def next_migration_number(dirname)
+          next_migration_number = current_migration_number(dirname) + 1
+          ActiveRecord::Migration.next_migration_number(next_migration_number)
+        end
+      end
 
       source_root File.expand_path('../templates', __FILE__)
 
