6b107e0de141ecd4e77dea607e2449e711a9e7e1
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-06-09 09:28:32 +0300

    load active_record helper files directly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'black_and_white'
-require 'support/active_record/object'
-#Dir['support/**/*.rb'].each { |f| require f }
+# Find gem root path
+Dir[[File.expand_path('../', __FILE__), 'support/**/*.rb'].join('/')].each { |f| require f }
 
 RSpec.configure do |config|
 end
