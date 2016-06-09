554706859a4c5b6f141f694efb9d2eaec23334da
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-06-09 09:19:41 +0300

    include active_record stuff if defined

--- lib/black_and_white.rb
+++ lib/black_and_white.rb
@@ -2,13 +2,6 @@ require "black_and_white/version"
 require "black_and_white/config"
 
 module BlackAndWhite
- # if defined?(ActiveRecord)
- #   require 'black_and_white/active_record'
- # elsif defined?(Mongoid)
- #   require 'black_and_white/mongoid.rb'
- # else
- #   raise 'Black and White is designed to work with ActiveRecord or Mongoid.'
- # end
 
   def self.configure
     yield(config)
@@ -16,5 +9,9 @@ module BlackAndWhite
 
   def self.config
     @config ||= Config.new
+  end
+
+  if defined?(::ActiveRecord)
+    require "black_and_white/active_record/participant"
   end
 end
