2ea8161d4d29afeba0bf31051562a1f2724ac8ca
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-09-23 18:53:04 +0300

    fix typo in initializer

--- lib/generators/black_and_white/templates/black_and_white_config.rb
+++ lib/generators/black_and_white/templates/black_and_white_config.rb
@@ -1,7 +1,7 @@
 BlackAndWhite.configure do |config|
   config.bw_class = 'User'
   config.bw_class_table = :users
-  config.bw_main_table = :ab_test
+  config.bw_main_table = :ab_tests
   config.bw_join_table = :ab_tests_users
 end
 BlackAndWhite::Hooks.init
